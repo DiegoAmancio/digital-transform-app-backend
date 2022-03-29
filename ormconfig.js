e1bfd702b06f1bd137e56e1fe0b4c54546d72ec4
DiegoAmancio <diego.pereira@ccc.ufcg.edu.br>
2022-03-29 17:53:43 -0300

    refactor: db_url

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,6 @@
 module.exports = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT || 5432,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
   entities: ['dist/modules/**/infra/database/*.entity.js'],
   migrations: ['./dist/shared/migrations/*.migration.js'],
   cli: {
